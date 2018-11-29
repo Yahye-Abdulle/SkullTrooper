345e7fd9067f3956bd83e0cc387c1c7cb2ef8c12
Yahye Abdulle <35307367+Yahye-Abdulle@users.noreply.github.com>
2018-11-29 10:38:30 +0000

    Create preload.js

---
+++ preload.js
@@ -0,0 +1,30 @@
+function load() {
+	cursor_image = loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/OpenMoji-black_2196.svg/512px-OpenMoji-black_2196.svg.png');
+	player_image = loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/OpenMoji-color_23FA.svg/512px-OpenMoji-color_23FA.svg.png')
+	circle_image = loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Android_Emoji_1f35c.svg/512px-Android_Emoji_1f35c.svg.png');
+	background_one = loadImage('https://i.imgur.com/VKTPd2M.jpg');
+	background_two = loadImage("https://openprocessing-usercontent.s3.amazonaws.com/files/user143705/visual609616/hcf6fea9dcc4fd568d6854e01729a7707/city-2444516_960_720.jpg");
+	text_font = loadFont('https://openprocessing-usercontent.s3.amazonaws.com/files/user143705/visual609616/hcf6fea9dcc4fd568d6854e01729a7707/ArchivoNarrow-Bold.ttf');
+	right_arrow_image = loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Android_Emoji_25b6.svg/512px-Android_Emoji_25b6.svg.png');
+	left_arrow_image = loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Android_Emoji_25c0.svg/512px-Android_Emoji_25c0.svg.png');
+	light_one = loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Emojione_BW_26A1.svg/512px-Emojione_BW_26A1.svg.png');
+	right_one = loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Emojione_26A1.svg/512px-Emojione_26A1.svg.png');
+	
+	choose_image = loadImage("https://openprocessing-usercontent.s3.amazonaws.com/files/user143705/visual609616/hcf6fea9dcc4fd568d6854e01729a7707/download.png")
+	
+	mouse_hover = loadSound("https://openprocessing-usercontent.s3.amazonaws.com/files/user143705/visual609616/hcf6fea9dcc4fd568d6854e01729a7707/166186__drminky__menu-screen-mouse-over.wav");
+	
+	sniper_shot = loadSound("https://openprocessing-usercontent.s3.amazonaws.com/files/user143705/visual609616/hcf6fea9dcc4fd568d6854e01729a7707/108852__emsiarma__snipershot.wav");
+	
+	select_ball_one = loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Emojione_BW_23FA.svg/512px-Emojione_BW_23FA.svg.png');
+	select_ball_two = loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Emojione_23FA.svg/512px-Emojione_23FA.svg.png');
+	
+	zombie = loadImage("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Noto_Emoji_Oreo_1f9df_200d_2642.svg/128px-Noto_Emoji_Oreo_1f9df_200d_2642.svg.png");
+	zombie_2 = loadImage("https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Twemoji2_1f9df.svg/48px-Twemoji2_1f9df.svg.png");
+	
+	zombie_music = loadSound("https://openprocessing-usercontent.s3.amazonaws.com/files/user143705/visual609616/hcf6fea9dcc4fd568d6854e01729a7707/449113__skibkamusic__skibka-music-music-sex-game.mp3");
+	
+	// blood_spatter = createImg("https://openprocessing-usercontent.s3.amazonaws.com/files/user143705/visual609616/hcf6fea9dcc4fd568d6854e01729a7707/giphy.gif")
+	
+	// player_list = loadJSON('https://drive.google.com/open?id=161WSCo8IF6HxvTsvlZIF1gDr-bhF1QC7');
+}
